fdbe4c10007cdc9e66d983c05d4bfc147e099f30
Jean Haro <JeanCHL98@hotmail.com>
2025-01-19 13:01:44 -0500

    Creando componentes de header y footer

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// -> Layout
 import { LayoutComponent } from './layout/layout.component';
+// -> Components Layout
+import { HeaderComponent } from './layout/components/header/header.component';
+import { FooterComponent } from './layout/components/footer/footer.component';
+
+// Pages
 import { CalendarComponent } from './pages/calendar/calendar.component';
 import { DatesComponent } from './pages/dates/dates.component';
-import { ListComponent } from './pages/list/list.component';
-import { GalleryComponent } from './pages/gallery/gallery.component';
+// Shared Pages
 import { SearchComponent } from './pages/shared/search/search.component';
-
-
 
 @NgModule({
   declarations: [
     LayoutComponent,
     CalendarComponent,
     DatesComponent,
-    ListComponent,
-    GalleryComponent,
-    SearchComponent
+    SearchComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
