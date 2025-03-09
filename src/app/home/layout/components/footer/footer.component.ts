fac7425e1e276dbe17bd2e98a7fc588381d90dbd
Jean Haro <JeanCHL98@hotmail.com>
2025-03-08 23:59:29 -0500

    Footer estructurado y estilizado

--- src/app/home/layout/components/footer/footer.component.ts
+++ src/app/home/layout/components/footer/footer.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+// FontAwesome
+import {
+  IconDefinition,
+  faSquareFacebook,
+  faSquareInstagram,
+  faSquareGithub,
+  faAngular,
+  faNode
+} from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'home-footer',
   standalone: false,
@@ -8,5 +17,12 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  // Atributos
+  faSquareFacebook: IconDefinition = faSquareFacebook;
+  faSquareInstagram: IconDefinition = faSquareInstagram;
+  faSquareGithub: IconDefinition = faSquareGithub;
+  faAngular: IconDefinition = faAngular;
+  faNode: IconDefinition = faNode;
 
+  year = new Date().getFullYear();
 }
